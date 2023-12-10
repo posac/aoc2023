4851e280cf6ada4d26c7e2ffb9f360aa021690b2
andrzej.developer <andrzej.developer@gmail.com>
2023-12-10 14:28:27 +0100

    Day 3 - part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -14,29 +14,31 @@ fun main() {
 val PART_NUMBER_REGEX = "(?<partNumber>\\d+)".toRegex()
 val SYMBOL_REGEX = "(?<symbol>[^.0-9\\n])".toRegex()
 
-sealed interface LineParts {
-    val lineNumber: Int
 
-    data class PartNumber(val number: Int, override val lineNumber: Int, val position: IntRange) : LineParts
-    data class Symbol(val value: String, override val lineNumber: Int, val position: Int) : LineParts
-}
+data class PartNumber(val number: Int, val lineNumber: Int, val position: IntRange)
+data class Symbol(val value: String, val lineNumber: Int, val position: Int)
 
 private fun part1(input: List<String>): Int {
 
-    return processLines(input, false)
+    return processLines(input) {
+        value.sumOf { it.number }
+    }
 }
 
-private fun processLines(input: List<String>, multiply: Boolean): Int {
-    return input.foldRightIndexed(mutableListOf<LineParts.PartNumber>() to mutableListOf<LineParts.Symbol>()) { index, line, acc ->
+private fun processLines(
+    input: List<String>,
+    calculate: Map.Entry<Symbol, List<PartNumber>>.() -> Int
+): Int {
+    return input.foldRightIndexed(mutableListOf<PartNumber>() to mutableListOf<Symbol>()) { index, line, acc ->
         val parts = PART_NUMBER_REGEX.findAll(line).map {
-            LineParts.PartNumber(
+            PartNumber(
                 number = it.value.toInt(),
                 lineNumber = index,
                 position = it.range
             )
         }.toList()
         val symbols = SYMBOL_REGEX.findAll(line).map {
-            LineParts.Symbol(
+            Symbol(
                 value = it.value,
                 lineNumber = index,
                 position = it.range.first.apply { require(it.range.first == it.range.last) }
@@ -53,21 +55,10 @@ private fun processLines(input: List<String>, multiply: Boolean): Int {
             }
         }
     }.entries.sumOf {
-        it.calculateRatios(multiply)
+        it.calculate()
     }
 }
 
-private fun Map.Entry<LineParts.Symbol, List<LineParts.PartNumber>>.calculateRatios(multiply: Boolean = false): Int {
-    return if (multiply.not())
-        value.sumOf { it.number }
-    else {
-        when {
-            key.value == "*" && value.size == 2 -> value[1].number * value[0].number
-            else -> 0
-        }
-    }
-
-}
 
 private fun checkPart1() {
     val partOneTest = readInput("${dayName}_test")
@@ -76,8 +67,13 @@ private fun checkPart1() {
 
 private fun checkPart2() {
     val partTwoTest = readInput("${dayName}_test")
-    check(part2(partTwoTest).println("Part two test result") == 467835 )
+    check(part2(partTwoTest).println("Part two test result") == 467835)
 }
 
-private fun part2(input: List<String>) = processLines(input, true)
+private fun part2(input: List<String>) = processLines(input) {
+    when {
+        key.value == "*" && value.size == 2 -> value[1].number * value[0].number
+        else -> 0
+    }
+}
 

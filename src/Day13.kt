8ba3e4745e0b4c5708183be5dfb320409a290f0e
Posac <mikio90@gmail.com>
2023-12-17 19:40:15 +0100

    Day 13 - part 1

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,3 +1,5 @@
+import kotlin.math.ceil
+
 private const val DAY_NAME = "Day13"
 fun main() {
     checkPart1()
@@ -10,38 +12,86 @@ fun main() {
 }
 
 object Day13 {
-    data class Row(val id: Int, val values: List<Char>){
-        fun getString() = values.joinToString("")
+    interface Container {
+        val id: Int
+        fun getString(): String
     }
-    data class Column(val id: Int, val values: MutableList<Char>){
-        fun getString() = values.joinToString("")
+
+    data class Row(override val id: Int, val values: List<Char>) : Container {
+        override fun getString() = values.joinToString("")
+    }
+
+    data class Column(override val id: Int, val values: MutableList<Char>) : Container {
+        override fun getString() = values.joinToString("")
     }
 }
 
 private fun part1(inputData: List<String>): Long {
-    val it = sequence<List<String>> {
+    val batchedInput = sequence<List<String>> {
         var batch = mutableListOf<String>()
         inputData.forEach {
-            it.isBlank()
-        }
-    }
-    val columns = input.first().mapIndexed { index, c -> Day13.Column(id = index, values = mutableListOf()) }
-    val rows = input.mapIndexed { index, s ->
-        Day13.Row(id = index, values = s.mapIndexed { index, c ->
-            columns[index].values.add(c)
-            c
-        })
-    }
-    columns.sortedBy { it.getString() }.forEach {
-        it.println()
-    }
+            if (it.isEmpty()) {
+                yield(batch)
+                batch = mutableListOf()
+            } else {
+                batch.add(it)
+            }
 
-    return input.size.toLong()
+        }
+        yield(batch)
+    }.toList()
+    return batchedInput.map { input ->
+        val columns = input.first().mapIndexed { index, c -> Day13.Column(id = index, values = mutableListOf()) }
+        val rows = input.mapIndexed { index, s ->
+            Day13.Row(id = index, values = s.mapIndexed { index, c ->
+                columns[index].values.add(c)
+                c
+            })
+        }
+        val duplicatedColumns = checkDuplication(columns)
+        val duplicatedRows = checkDuplication(rows)
+        println("-----")
+        duplicatedColumns.sum() + duplicatedRows.sumOf { it * 100 }
+    }.sum()
+
+}
+
+private fun checkDuplication(items: List<Day13.Container>): List<Long> {
+    if (
+        items.drop(1).none { it.getString() == items.first().getString() } &&
+        items.dropLast(1).none { it.getString() == items.last().getString() }
+    )
+        return emptyList()
+    val duplicatedPairs =
+        items.groupBy { it.getString() }.filter { it.value.size >= 2 }.flatMap {
+
+            if (it.value.size == 2)
+                listOf(it.value.map { it.id }.sorted())
+            else
+                it.value.combination().map { listOf(it.first.id, it.second.id).sorted() }
+        }
+            .toList()
+
+    check(duplicatedPairs.filter { it.size > 2 }
+        .isEmpty()) { "There are three items duplicated ${duplicatedPairs.filter { it.size > 2 }.first().toString()}}" }
+
+    return duplicatedPairs.map { (it[1] + it[0]) / 2.0 to it }
+        .groupBy({ it.first }) { it.second }
+        .filter {
+            it.value.map { it[1] }.sorted() == IntRange(ceil(it.key).toInt(), items.size - 1).toList()
+                    || it.value.map { it[0] }.sorted() == IntRange(0, it.key.toInt()).toList()
+        }
+        .map {
+            ceil(it.key).toLong()
+        }
+
 }
 
 private fun checkPart1() {
     val partOneTest = readInputResources(DAY_NAME, "test")
     check(part1(partOneTest).println("Part one test result") == 405L)
+    check(part1(readInputResources(DAY_NAME, "test_2")).println("Part one test result") == 1400L)
+    check(part1(readInputResources(DAY_NAME, "input")).println("Part one test result") == 35538L)
 }
 
 private fun checkPart2() {
